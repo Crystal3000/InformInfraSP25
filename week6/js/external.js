387c1baf8ac11a58009c1c43a23ed145d4d5831a
Crystal Stewart <crstewa@iu.edu>
2025-02-23 15:50:52 -0500

    Week Something

---
+++ week6/js/external.js
@@ -0,0 +1,23 @@
+console.log("Umm...where are we");
+
+const birthYear = 1287;
+
+let myName = "Ty the Noodle Arm Guy";
+
+console.log(myName, "was born", birthYear);
+
+myName = "Ty the Fly Guy";
+
+
+// constant variables can't change their values
+// birthYear = 1208;
+
+console.warn("My name maye have devoured 10 souls");
+
+console.log("myName is", myName.length, "characters long.");
+
+console.log("myName include ty", myName.toUpperincludes (ty));
+
+const PI=3.14
+
+console.log(PI.tofixed(2));
