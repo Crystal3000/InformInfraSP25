cd86846e8dc08c3e9c89311b18a5dabc9ce14ac0
Crystal Stewart <crstewa@iu.edu>
2025-03-13 11:50:16 -0400

    Midterm

---
+++ midterm/catalog.js
@@ -0,0 +1,11 @@
+const catalogRef = document.querySelector ("#catalog");
+
+const catalogItems = [
+    {image: "midterm-images/Midterm/i210-midterm-product1.webp", name: "Product 1",},
+    {image: "midterm-images/Midterm/i210-midterm-product2.webp", name: "Product 2",},
+    {image: "midterm-images/Midterm/i210-midterm-product3.webp", name:"Product 3",},
+    {image: "midterm-images/Midterm/i210-midterm-product4.webp", name: "Product 4",},
+    {image: "midterm-images/Midterm/i210-midterm-product5.webp", name: "Product 5",},
+    {image: "midterm-images/Midterm/i210-midterm-product6.webp", name: "Product 6",},
+
+]
